d0369482829c46e42ad2cf6a916de7124960b527
Zinx140 <frostocean4@gmail.com>
2024-12-24 17:28:34 +0700

    innit commit

--- main.cpp
+++ main.cpp
@@ -4,12 +4,21 @@
 #include <windows.h>
 #include <time.h>
 #include <vector>
-// sudah jalan cuma menunya tak tahan dulu soalnya rusak >3
+#include <conio.h>
+#include <windows.h>
+#include <fstream>
+#include <algorithm>
+#include <iomanip>
+
 using namespace std;
 
 // Ukuran arena
 const int width = 25;
 const int height = 20;
+int score = 0;
+string playerName;
+vector<string> playerNames;
+vector<int> scores;
 
 // Tetromino menggunakan vector
 vector<vector<vector<int>>> tetromino = {
@@ -20,6 +29,24 @@ vector<vector<vector<int>>> tetromino = {
     {{0, 0, 1}, {1, 1, 1}},          // J
     {{0, 1, 1}, {1, 1, 0}},          // S
     {{1, 1, 0}, {0, 1, 1}}           // Z
+};
+
+void setColor(int color) {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, color);
+}
+
+int arenaColors[height][width] = {0};
+
+// set warnanya ganti aja angkanya kalo mau ganti warna (1-15)
+int tetrominoColors[7] = {
+    9,  // I - Light-Blue
+    12, // O - Light-Red
+    13, // T - Magenta
+    6,  // L - Light Red
+    3,  // J - Aqua
+    2,  // S - Green
+    4   // Z - Red
 };
 
 // Fungsi untuk memutar tetromino ke kanan (90 derajat searah jarum jam)
@@ -34,9 +61,6 @@ vector<vector<int>> rotateMatrix(const vector<vector<int>> &matrix) {
     }
     return rotated;
 }
-
-int candrop[width];
-
 
 // Fungsi untuk memeriksa apakah tetromino bisa diputar
 bool canRotate(int arena[height][width], int position_x, int position_y, vector<vector<int>> &rotatedTetromino) {
@@ -57,7 +81,7 @@ bool canRotate(int arena[height][width], int position_x, int position_y, vector<
 }
 
 // Fungsi untuk menggambar tetromino di dalam arena
-void summonTetromino(int arena[height][width], int position_x, int position_y, vector<vector<int>> &currentTetromino) {
+void summonTetromino(int arena[height][width], int arenaColors[height][width], int position_x, int position_y, vector<vector<int>> &currentTetromino, int colorIndex) {
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
             if (i == 0 || j == 0 || i == height - 1 || j == width - 1) {
@@ -65,6 +89,7 @@ void summonTetromino(int arena[height][width], int position_x, int position_y, v
             } else {
                 if (arena[i][j] != 7) {
                     arena[i][j] = 0; // Arena kosong
+                    arenaColors[i][j] = 0; // Reset color
                 }
             }
         }
@@ -73,57 +98,57 @@ void summonTetromino(int arena[height][width], int position_x, int position_y, v
         for (int j = 0; j < currentTetromino[i].size(); j++) {
             if (currentTetromino[i][j] == 1) {
                 arena[position_y + i][position_x + j] = currentTetromino[i][j];
+                arenaColors[position_y + i][position_x + j] = tetrominoColors[colorIndex];
             }
         }
     }
 }
 
 // Fungsi untuk menggambar arena
-void draw(int arena[height][width], vector<vector<int>> &nextTetromino) {
+void draw(int arena[height][width], int arenaColors[height][width], vector<vector<int>> &nextTetromino, int currentTetrominoIndex, int nextTetrominoIndex) {
     const int boxHeight = 4;
     const int boxWidth = 4;
-    const int horizontalSpacing = 5; // Spacing diantara arena dan "Next Tetromino"
+    const int horizontalSpacing = 5;
 
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
             if (arena[i][j] == 1 || arena[i][j] == 7) {
-                cout << "@"; // Tetromino
+                setColor(arenaColors[i][j]); // ngeset warna tetromino
+                cout << "@";
+                setColor(7); // Reset color ke default
             } else if (arena[i][j] == 2) {
-                cout << "#"; // Border
+                cout << "#";
             } else if (arena[i][j] == 4) {
+                setColor(arenaColors[i][j]);
                 cout << ".";
+                setColor(7);
             } else {
-                cout << " "; // Arena kosong
+                cout << " ";
             }
         }
 
-        // Spacing diantara arena dan "Next Tetromino"
+        // Draw the "Next Tetromino" box
         cout << string(horizontalSpacing, ' ');
-
-        // Print "Next Tetromino" label di atas kotak
-        if (i == (height / 2 - boxHeight / 2 - 2)) {
-            // Center
-            int labelPadding = max(0, (boxWidth - 15) / 2);
-            cout << string(labelPadding, ' ') << "Next Tetromino:";
+        if(i == 1) {
+            cout << "Score : " << score;
         }
-
-        // Print "Next Tetromino" box baris per baris
+        if (i == (height / 2 - boxHeight / 2 - 2)) {
+            cout << "Next Tetromino:";
+        }
         if (i >= (height / 2 - boxHeight / 2) && i < (height / 2 + boxHeight / 2)) {
-            int tetrominoRow = i - (height / 2 - boxHeight / 2); // Calculate current row untuk tetromino
-
-            // left border
+            int tetrominoRow = i - (height / 2 - boxHeight / 2);
             cout << "#";
 
             if (tetrominoRow < nextTetromino.size()) {
-                // Center tetromino dalam box 4x4
                 int paddingLeft = (boxWidth - nextTetromino[tetrominoRow].size()) / 2;
                 int paddingRight = boxWidth - nextTetromino[tetrominoRow].size() - paddingLeft;
-
-                // Print left padding, tetromino contents, dan right padding
                 cout << string(paddingLeft, ' ');
+
                 for (int j = 0; j < nextTetromino[tetrominoRow].size(); j++) {
                     if (nextTetromino[tetrominoRow][j] == 1) {
+                        setColor(tetrominoColors[nextTetrominoIndex]); // Color for next tetromino
                         cout << "@";
+                        setColor(7); // Reset
                     } else {
                         cout << " ";
                     }
@@ -132,22 +157,20 @@ void draw(int arena[height][width], vector<vector<int>> &nextTetromino) {
             } else {
                 cout << string(boxWidth, ' ');
             }
-
-            // box's right border
             cout << "#";
         } else if (i == (height / 2 - boxHeight / 2 - 1) || i == (height / 2 + boxHeight / 2)) {
-            cout << string(boxWidth + 2, '#'); // Full-width border
+            cout << string(boxWidth + 2, '#');
         }
-
         cout << endl;
     }
 
-    // instructions
+    // Instructions
     cout << endl;
     cout << "'A' to move tetromino to the left" << endl;
     cout << "'D' to move tetromino to the right" << endl;
     cout << "'W' to rotate tetromino" << endl;
 }
+
 
 bool canMove(int arena[height][width], int position_x, int position_y, vector<vector<int>> &currentTetromino) {
     int newHeight = currentTetromino.size();
@@ -206,6 +229,7 @@ void clearLines(int arena[height][width]) {
             }
         }
         if (fullLine) {
+            score += 100;
             for (int j = 1; j < width - 1; j++) {
                 arena[i][j] = 0;
             }
@@ -227,7 +251,8 @@ bool gameOver(int arena[height][width]) {
     return false;
 }
 
-void hardDrop(int arena[height][width], vector<vector<int>> currentTetromino, int position_x, int position_y, bool drop, int &temp) {
+void hardDrop(int arena[height][width], vector<vector<int>> currentTetromino, int position_x, int position_y, bool drop, int &temp, int randomTetromino) {
+    int shadowColor = tetrominoColors[randomTetromino];
     while (canMove(arena, position_x, position_y, currentTetromino)) {
         if (canMove(arena, position_x, position_y, currentTetromino)) {
             position_y++;
@@ -239,6 +264,7 @@ void hardDrop(int arena[height][width], vector<vector<int>> currentTetromino, in
             if (currentTetromino[i][j] == 1) {
                 if (arena[position_y + i - 1][position_x + j] != 1 && arena[position_y + i - 1][position_x + j] != 7 && arena[position_y + i - 1][position_x + j] != 2) {
                     arena[position_y + i - 1][position_x + j] = 4;
+                    arenaColors[position_y + i - 1][position_x + j] = shadowColor; // Set color for shadow
                 }
             }
             if (drop) {
@@ -248,8 +274,209 @@ void hardDrop(int arena[height][width], vector<vector<int>> currentTetromino, in
     }
 }
 
+int main_menu() {
+    int control_x = 0; // Position of ">"
+    string arr_mainmenu[3] = {"Play", "Leaderboard", "Exit"};
+    char input;
+    const int menu_size = 3;
+
+    // Get console window dimensions
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    int console_width, console_height; //idk how tf does this even work, don't bother asking me
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+    console_width = csbi.dwSize.X;
+    console_height = csbi.dwSize.Y;
+
+    const int menu_width = 60; // menu width
+
+    do {
+        system("cls");
+
+        // hitung horizontal padding
+        string horizontal_padding((console_width - menu_width) / 2, ' '); // Spaces for horizontal centering
+
+        cout << horizontal_padding << "############################################################" << endl;
+        cout << horizontal_padding << "#                                                          #" << endl;
+        cout << horizontal_padding << "#        @@@@@@  @@@@@  @@@@@@  @@@@  @@  @@@@@@@          #" << endl;
+        cout << horizontal_padding << "#        @@@@@@  @      @@@@@@  @  @  @@  @@               #" << endl;
+        cout << horizontal_padding << "#          @@    @@@@@    @@    @@@@  @@  @@@@@@@          #" << endl;
+        cout << horizontal_padding << "#          @@    @@@@@    @@    @@    @@  @@@@@@@          #" << endl;
+        cout << horizontal_padding << "#          @@    @        @@    @ @   @@       @@          #" << endl;
+        cout << horizontal_padding << "#          @@    @@@@@    @@    @  @  @@  @@@@@@@          #" << endl;
+        cout << horizontal_padding << "#                                                          #" << endl;
+
+        // kontrol untuk optionya
+        for (int i = 0; i < menu_size; i++) {
+            cout << horizontal_padding << "#               ";
+            if (control_x == i) {
+                cout << "> " << arr_mainmenu[i];
+            } else {
+                cout << "  " << arr_mainmenu[i];
+            }
+            cout << string(41 - arr_mainmenu[i].length(), ' ') << "#" << endl;
+        }
+
+        cout << horizontal_padding << "#                                                          #" << endl;
+        cout << horizontal_padding << "#       Press W or S to move, press ENTER to choose        #" << endl;
+        cout << horizontal_padding << "############################################################" << endl;
+
+        input = _getch();
+        if (input == 'w' || input == 'W') {
+            if (control_x > 0) control_x--; // Move up
+        } else if (input == 's' || input == 'S') {
+            if (control_x < menu_size - 1) control_x++; // Move down
+        }
+
+    } while (input != '\r'); // ini adalah ascii untuk 'ENTER', dia berhenti kalo enter di tekan
+
+    return control_x;
+}
+
+void loadScores() {
+    ifstream infile("leaderboard.txt");
+    string name;
+    int score;
+
+    if (!infile) {
+        cout << "Failed to open leaderboard file." << endl;
+        return;
+    }
+
+    while (infile >> name >> score) {
+        playerNames.push_back(name);
+        scores.push_back(score);
+    }
+    infile.close();
+}
+
+
+void saveScores() {
+    ofstream outfile("leaderboard.txt");
+
+    for (int i = 0; i < playerNames.size(); ++i) {
+        outfile << playerNames[i] << " " << scores[i] << endl;
+    }
+    outfile.close();
+}
+
+void saveScore(int score, const string& playerName) {
+    bool playerExists = false;
+    for (int i = 0; i < playerNames.size(); ++i) {
+        if (playerNames[i] == playerName) {
+            scores[i] += score;
+            playerExists = true;
+            break;
+        }
+    }
+
+    if (!playerExists) {
+        playerNames.push_back(playerName);
+        scores.push_back(score);
+    }
+
+    for (size_t i = 0; i < scores.size(); ++i) {
+        for (size_t j = i + 1; j < scores.size(); ++j) {
+            if (scores[i] < scores[j]) {
+                swap(scores[i], scores[j]);
+                swap(playerNames[i], playerNames[j]);
+            }
+        }
+    }
+
+    saveScores();
+}
+
+void showLeaderboard() {
+    ifstream inFile("leaderboard.txt");
+    string name;
+    int score;
+    int count = 1;
+    string topName;
+    int topScore = 0;
+
+    if (!inFile) {
+        cout << "Failed to open leaderboard file." << endl;
+        Sleep(2000);
+        return;
+    }
+
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    int console_width, console_height;
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+    console_width = csbi.dwSize.X;
+    console_height = csbi.dwSize.Y;
+
+    const int menu_width = 60;
+
+    system("cls");
+
+    string horizontal_padding((console_width - menu_width) / 2, ' ');
+
+    cout << horizontal_padding << "=========================================================" << endl;
+    cout << horizontal_padding << "| o                                                   o |" << endl;
+    cout << horizontal_padding << "|      @      @@@     @@@     @@@      @@@     @@@      |" << endl;
+    cout << horizontal_padding << "|      @      @@      @@@     @  @     @@      @@@      |" << endl;
+    cout << horizontal_padding << "|      @@@    @@@     @ @     @@@      @@@     @  @     |" << endl;
+    cout << horizontal_padding << "| o                                                   o |" << endl;
+    cout << horizontal_padding << "|          @@@@    @@@     @@@     @@@      @@@         |" << endl;
+    cout << horizontal_padding << "|          @ @     @ @     @@@     @@@      @  @        |" << endl;
+    cout << horizontal_padding << "|          @@@@    @@@     @ @     @  @     @@@         |" << endl;
+    cout << horizontal_padding << "| o                                                   o |" << endl;
+    cout << horizontal_padding << "=========================================================" << endl;
+
+    cout << endl;
+
+    cout << horizontal_padding <<"+=======================================================+" << endl;
+    cout << horizontal_padding <<"|                 TOP 5 HIGHEST SCORERS                 |" << endl;
+    cout << horizontal_padding <<"+=======================================================+" << endl;
+    cout << horizontal_padding <<"| " << left << setw(4) << "No"
+         << "| " << left << setw(15) << "Name"
+         << "| " << right << setw(10) << "Score" << "   |                 |" << endl;
+    cout << horizontal_padding <<"+=======================================================+" << endl;
+
+    while (inFile >> name >> score && count <= 5) {
+        cout << horizontal_padding <<"| " << left << setw(4) << count
+             << "| " << left << setw(15) << name
+             << "| " << right << setw(10) << score << "   |                 |" << endl;
+
+        if (score > topScore) {
+            topScore = score;
+            topName = name;
+        }
+
+        count++;
+    }
+
+    while (count <= 5) {
+        cout << horizontal_padding <<"| " << left << setw(4) << count
+             << "| " << left << setw(15) << "-"
+             << "| " << right << setw(10) << "0" << "   |                 |" << endl;
+        count++;
+    }
+
+    cout << horizontal_padding <<"+=======================================================+" << endl;
+
+    if (!topName.empty()) {
+        cout << horizontal_padding <<"| " << left << setw(9) << "              Congratulation to " << topName << "             |" << endl;
+        cout << horizontal_padding <<"| " << left << setw(9) << "              Your highest score is " << topScore << "               |"<< endl;
+    }
+
+    cout << horizontal_padding << "+=======================================================+" << endl;
+    cout << horizontal_padding <<"|          Press any key to return to menu...           |" << endl;
+    cout << horizontal_padding <<"+=======================================================+" << endl;
+
+    inFile.close();
+    getch();
+}
+
 int main() {
     srand(time(0));
+
+    while (true){
+        int selection = main_menu();
+
+        if (selection==0){ //if selection==0, then start game!
+
     int dx = 0;
     int tetromino_y = 1;
     int tetromino_x = rand() % 19 + 1;
@@ -260,6 +487,17 @@ int main() {
     int arena[height][width] = {0};
     bool flag = true;
     int tempY;
+    score = 0;
+
+    system("cls");
+
+    cout << "Enter Your Name : ";
+    cin >> playerName;
+
+    cout << "Have Fun Playing!" << endl;
+    Sleep(1500);
+
+    system("cls");
 
     while (!gameOver(arena)) {
         bool drop = false;
@@ -288,7 +526,7 @@ int main() {
                 }
             }
 
-            summonTetromino(arena, tetromino_x, tetromino_y, currentTetromino);
+            summonTetromino(arena, arenaColors, tetromino_x, tetromino_y, currentTetromino, randomTetromino);
             tetromino_y++;
         } else {
             if (!flag) {
@@ -306,25 +544,39 @@ int main() {
                 for (int j = 0; j < currentTetromino[i].size(); j++) {
                     if (currentTetromino[i][j] == 1) {
                         arena[tetromino_y + i - 1][tetromino_x + j] = 7;
+                        arenaColors[tetromino_y + i - 1][tetromino_x + j] = tetrominoColors[randomTetromino];
                     }
                 }
             }
 
-
             tetromino_y = 1;
             tetromino_x = rand() % 19 + 1;
-            currentTetromino = nextTetromino; // Set current tetromino to the next one
-            nextTetrominoIndex = rand() % 7; // Ambil tetronimo berikutnya
-            nextTetromino = tetromino[nextTetrominoIndex]; // Set next tetromino
+            randomTetromino = nextTetrominoIndex; // Update the current Tetromino index
+            currentTetromino = nextTetromino;
+            nextTetrominoIndex = rand() % 7;     // Generate tindex tetronimo berikutnya
+            nextTetromino = tetromino[nextTetrominoIndex]; // Set next Tetromino
             flag = true;
         }
-        hardDrop(arena, currentTetromino, tetromino_x, tetromino_y, drop, tempY);
-        draw(arena, nextTetromino);
+
+        hardDrop(arena, currentTetromino, tetromino_x, tetromino_y, drop, tempY, randomTetromino);
+        draw(arena, arenaColors, nextTetromino, randomTetromino, nextTetrominoIndex);
         clearLines(arena);
         Sleep(150);
         system("cls"); // Bersihkan layar untuk menggambar ulang
     }
 
     cout << "Game Over!" << endl;
+    loadScores();
+    saveScore(score, playerName);
+    Sleep(2000);
+
+        } else if (selection==1){
+            showLeaderboard();
+        } else if (selection==2){
+            break;
+        }
+    }
+
+
     return 0;
 }
